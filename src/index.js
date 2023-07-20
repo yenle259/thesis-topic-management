324c3f644995baf276ef571b01e44e567bc1d242
yenle259 <lebuihongyen@gmail.com>
2023-07-20 23:41:39 +0700

    feat: install morgan

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const morgan = require('morgan');
 const app = express()
 const port = 3000
+
+//HTTP logger
+// app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
